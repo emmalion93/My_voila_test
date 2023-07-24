157be26f05bb72bc3d5b2f4e32d85e346a923cfc
Emily J Turner <90275810+emmalion93@users.noreply.github.com>
2023-07-24 15:15:48 -0400

    Update ISEEU.py

--- content/ISEEU.py
+++ content/ISEEU.py
@@ -186,7 +186,7 @@ class MultiQuery(widgets.VBox):
         self.add_button.on_click(self.add_button_clicked)
 
         self.add_button = widgets.Button(icon="minus", disabled=False, button_style='success', layout=FixedIconButtonLayout())
-        self.add_button.on_click(webbrowser.open('http://localhost/var/www/html/iseeu/Request', new=0, autoraise=true))
+        self.add_button.on_click(webbrowser.open('http://localhost/var/www/html/iseeu/Request', new=0, autoraise=True))
         
         self.layer_container = LayerContainer(self.ion_dropdown, self.ion_energy_selector)
         self.parent = parent
