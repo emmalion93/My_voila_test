b4590da64468ea4c91376465fbacc9b307013597
Emily J Turner <90275810+emmalion93@users.noreply.github.com>
2023-07-24 20:17:56 -0400

    Update ISEEU.py

--- content/ISEEU.py
+++ content/ISEEU.py
@@ -4,7 +4,7 @@ import warnings
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
 
-    from IPython.display import clear_output, display, HTML, Javascript
+    from IPython.display import clear_output, display, HTML
     import io
     import ipywidgets as widgets
     import matplotlib.pyplot as plt
